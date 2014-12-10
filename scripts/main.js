aa5513aaebdd1892365f823ac369693ef0e6243c
Gerald A. Wakefield <gwake1@gmail.com>
2014-12-10 11:32:16 -0600

    firebase and ng-repeat

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,16 @@
 (function() {
   "use strict";
-  
+  angular.module("myApp", ['ngRoute', 'mgcrea.ngStrap'])
+  .controller("AmazonController", function($http){
+    var vm = this;
+    $http.get("https://nsswishlist.firebaseio.com/Wishes.json")
+    .success(function(data){
+      vm.Wishes = data;
+      console.log("it works!");
+    })
+    .error(function(err){
+      console.log(err);
+    })
+
+  })
 }());
